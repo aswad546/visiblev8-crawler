f69b573c9c0b1333fac2c2ab8d71ef53414acbdb
aswad546 <aswadtariq05@gmail.com>
2024-07-17 20:41:19 +0000

    Modified crawler timeout

--- backend/app/api/api_v1/endpoints/tasks.py
+++ backend/app/api/api_v1/endpoints/tasks.py
@@ -99,7 +99,7 @@ class UrlSubmitRequestModel(BaseModel):
     disable_screenshot: Optional[bool] = False
     disable_artifact_collection: Optional[bool] = False
     parser_config: Optional[ParserConfigRequest]
-    hard_timeout: Optional[int] = 2 * 60 # timeout
+    hard_timeout: Optional[int] = 5 * 60 # timeout
 
 
 @dataclass
